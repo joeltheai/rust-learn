6eb567b3373da40c33581bd2634c3fd6354c975f
joeltheai <55612521+joeltheai@users.noreply.github.com>
2026-05-12 22:24:59 +0530

    Add Color enum for color representation and enhance Point and Rectangle implementations

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+#[derive(Debug)]
+enum Color {
+    Rgb(u8, u8, u8),
+    Hsv(u8, u8, u8),
+    Hsl(u8, u8, u8),
+    Cmyk(u8, u8, u8, u8),
+    Gray(u8),
+}
+
+
 struct Point {
     x: f32,
     y: f32,
 }
-
 impl Point {
     fn new(x: f32, y: f32) -> Self {
         Self { x, y }
     }
+}
+
+impl Point {
     fn distance_between(&self) -> f32 {
-        let x = self.x;
-        let y = self.y;
-        (x * x + y * y).sqrt()
+        (self.x * self.x + self.y * self.y).sqrt()
     }
 }
 
@@ -18,7 +28,6 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-
 impl Rectangle {
     fn new(width: u32, height: u32) -> Self {
         Self { width, height }
@@ -32,12 +41,17 @@ impl Rectangle {
         self.width = width;
     }
 
-    fn max(self, other: Rectangle) -> Rectangle {
+    fn max(self, other: Self) -> Self {
+        let w = self.width.max(other.width);
+        let h = self.height.max(other.height);
         Rectangle {
-            width: self.width.max(other.width),
-            height: self.height.max(other.height),
+            width: w,
+            height: h,
         }
     }
+    // fn set_to_max(&mut self, other: Rectangle) {
+    //     *self = self.max(other);
+    // }
 }
 
 fn main() {
@@ -57,5 +71,28 @@ fn main() {
 
     let max_rect = rect.max(rect2);
     println!("Max Rectangle -> ({}, {})", max_rect.width, max_rect.height);
+    
+
     println!("Area of max rectangle -> {}", max_rect.area());
+
+    let cloud = Color::Rgb(255, 255, 255);
+    let grass = Color::Hsv(100, 100, 100);
+    let water = Color::Hsl(100, 100, 100);
+    let earth = Color::Cmyk(100, 100, 100, 100);
+    let sky = Color::Gray(100);
+    println!("Cloud -> {:?}", cloud);
+    println!("Sky -> {:?}", sky);
+    println!("Grass -> {:?}", grass);
+    println!("Water -> {:?}", water);
+    println!("Earth -> {:?}", earth);
+    println!("Sky -> {:?}", sky);
+
+    println!("Cloud -> {:?}", match cloud {
+        Color::Rgb(r, g, b) => format!("RGB({r}, {g}, {b})"),
+        Color::Hsv(h, s, v) => format!("HSV({h}, {s}, {v})"),
+        Color::Hsl(h, s, l) => format!("HSL({h}, {s}, {l})"),
+        Color::Cmyk(c, m, y, k) => format!("CMYK({c}, {m}, {y}, {k})"),
+        Color::Gray(g) => format!("Gray({g})"),
+    });
+
 }
